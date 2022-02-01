36d743acd05aa06b9c53ead89f6e6d6fc19a7d1a
rebeccaleifer <rebecca.leifer15@gmail.com>
2022-02-01 16:29:31 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission6.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MakeAppointmentContext aptContext { get; set; }
+        public HomeController(MakeAppointmentContext someName)
+        {
+            aptContext = someName;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        public IActionResult ViewAppointments()
+        {
+            return View();
+        }
+    }
+}
